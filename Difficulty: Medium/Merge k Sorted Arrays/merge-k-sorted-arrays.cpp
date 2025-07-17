d45ddd1b27179d711f8ec8b243e9ecf581b75294
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-07-17 16:33:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+  public:
+    vector<int> mergeKArrays(vector<vector<int>>& arr, int K) {
+        vector<int> result;
+
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < K; j++) {
+                result.push_back(arr[i][j]);
+            }
+        }
+
+        sort(result.begin(), result.end());
+
+        return result;
+    }
+};
