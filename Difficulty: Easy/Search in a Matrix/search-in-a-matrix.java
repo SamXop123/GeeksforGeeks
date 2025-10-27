cdc2bcdc59cef3b4be20f6f89ca69d31507fa034
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-10-27 17:02:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a Matrix/search-in-a-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == x) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
